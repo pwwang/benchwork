ada9d0636efb6a59fd3822e50ab070a13f7b100a
pwwang <pwwang@pwwang.com>
2022-11-11 17:45:02 -0700

    🔖 0.0.2

--- benchwork/__init__.py
+++ benchwork/__init__.py
@@ -1,6 +1,12 @@
 from .api import BenchAPI
 from .case import BenchCase, BenchCaseVersion, BenchCaseSpeed
-from .set_ import BenchSet, BenchSetTable, BenchSetSpeed, BenchSetVersion
+from .set_ import (
+    BenchSet,
+    BenchSetTable,
+    BenchSetSpeed,
+    BenchSetVersion,
+    BenchSetMultiColTable,
+)
 from .suite import BenchSuite, run_suite
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
